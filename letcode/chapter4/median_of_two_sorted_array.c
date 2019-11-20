1fd9ed3d959e37343116528c32a31d4c9ca0deb6
zhangxa <zhangxa@localhost.localdomain>
2019-11-20 20:31:58 +0800

    modify median_of_two

--- letcode/chapter4/median_of_two_sorted_array.c
+++ letcode/chapter4/median_of_two_sorted_array.c
@@ -1,18 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int find_median_in_two_sorted_array(int *a, int *b, int na, int nb)
-{
-	int n = na + nb;
-	if(n%2==0)
-	{
-		return findKth(a, b, na, nb, n/2);
-	}
-	else
-	{
-		return (findKth(a, b, na, nb, n/2) + findKth(a, b, na, nb, n/2+1))/2;
-	}
-}
+#define min(a,b) ((a)<(b)? (a):(b))
 
 int findKth(int *a, int *b, int na, int nb, int k)
 {
@@ -25,20 +14,38 @@ int findKth(int *a, int *b, int na, int nb, int k)
 	if(nb==0)
 		return a[k];
 
-	int ma = na>>2;	
-	int mb = nb>>2;
-	if(a[ma]<=b[mb])
+	int ma = min(na, k/2) - 1;	
+	int mb = k - ma - 2;
+	if(a[ma]<b[mb])
 	{
-		if(ma-na+1>=k)
-			return a[k];
-		else
-			return (&a[ma+1], b, na-ma, nb, k-ma);
+			return (&a[ma+1], b, na-ma-1, nb, k-ma-1);
+	}
+	if(a[ma]>b[mb])	
+	{
+			return (a, &b[mb+1], na, nb-mb-1, k-mb-1);
+	}
+	return a[ma];
+}
+
+int find_median_in_two_sorted_array(int *a, int *b, int na, int nb)
+{
+	int n = na + nb;
+	if(n%2!=0)
+	{
+		return findKth(a, b, na, nb, n/2+1);
 	}
 	else
 	{
-		if(mb-nb+1>=k)
-			return b[k];
-		else
-			return (a, &b[mb+1], na, nb-mb, k-mb);
+		return (findKth(a, b, na, nb, n/2) + findKth(a, b, na, nb, n/2+1))/2;
 	}
+}
+
+
+int main()
+{
+	int a[]={1,2,3,4,5,12,13,14};
+	int b[]={2,3,4,5,8};
+
+	printf("[%d]\r\n",find_median_in_two_sorted_array(a,b,sizeof(a)/sizeof(int),sizeof(b)/sizeof(int)));
+	return 0;
 }
