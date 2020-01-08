77fdbec24ffcc87f7cc13de02c1bd6d2116967c4
happyAnger6 <happyAnger6@163.com>
2020-01-08 09:01:56 +0800

    add dqueue

---
+++ lg/commonly_used/dqueue.c
@@ -0,0 +1,65 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct dnode_{
+    int data;
+    struct dnode_ *next;
+    struct dnode_  *prev;
+}dnode;
+
+typedef struct dqueue_{
+    int len;
+    int cap;
+    dnode dq;
+}dqueue, *pDqueue;
+
+void dqueue_init();
+int dqueue_add_head(pDqueue dq, int data);
+int dqueue_add_tail(pDqueue dq, int data);
+int dqueue_del_head(pDqueue dq);
+int dqueue_del_tail(pDqueue dq);
+int dqueue_get_head(pDqueue dq);
+
+int dqueue_push(pDqueue dq, int a);
+{
+    if(dqueue_len(dq)==dq->cap)
+    {
+        dqueue_del_head(dq); 
+    }
+
+    for_each_dnode_from_tail(dq, d)
+    {
+        if(a>d->data)
+        {
+           dqueue_del_node(d);
+        }
+        else
+        {
+            break; 
+        } 
+    }
+}
+
+int main(int argc, char *argv[])
+{
+    int n = 0;
+    int k = 0;
+    int i;
+    if(argc<3)
+    {
+        printf("usage:%s <k> <nums...>\r\n");
+        return -1; 
+    }
+
+    k = atoi(argv[1]);
+    n = argc-2;
+    int *pNums = malloc(sizeof(int) * n);
+    for(i = 2; i < argc; i++)
+        pNums[i-2] = atoi(argv[i]);
+
+    for(i = 0; i < n; i++)
+    {
+        if(i==0)
+            init_dqueue(pNums, k); 
+    }
+}
